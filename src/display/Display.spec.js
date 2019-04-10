7415a3e2f7498f2be7e9327ac5e6f60d69324d7c
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-10 16:37:29 -0400

    tested if gate is open and unlocked in Display comp

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+describe('renders without crashing', () => {
+
+  test('renders the Display component', () => {
+    render(<Display />)
+  })
+
+  test('displays if gate is open and unlocked', ()=>{
+      const {getByText} = render(<Display locked={false} closed ={false} />)
+  })
+})
