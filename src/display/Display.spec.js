41a73ab1fa8b3c81cf827cdf6ff98d1fcfca8f1e
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-10 16:43:01 -0400

    All tests are done for Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -12,5 +12,26 @@ describe('renders without crashing', () => {
 
   test('displays if gate is open and unlocked', ()=>{
       const {getByText} = render(<Display locked={false} closed ={false} />)
+      getByText(/open/i);
+      getByText(/unlocked/i);
   })
-})
+
+  test('displays if gate being closed and locked', () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    getByText(/closed/i);
+    getByText(/locked/i);
+  })
+
+  test('when locked or closed use the red-led class', () => {
+    const { getByText } = render(<Display closed={true} locked={true} />);
+    const closedClass = getByText(/closed/i);
+    expect(closedClass).toHaveClass('red-led');
+  })
+
+  test('when unlocked or open use the green-led class', () => {
+    const { getByText } = render(<Display closed={false} locked={false} />);
+    const unlockedClass = getByText(/unlocked/i);
+    expect(unlockedClass).toHaveClass('green-led');
+  })
+
+});
